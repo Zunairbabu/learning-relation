a82922d17f7ba5d140663110b7d4680a0088cb42
Zunairbabu <41425252+Zunairbabu@users.noreply.github.com>
2019-02-19 19:37:09 +0500

    relation

---
+++ database/migrations/2019_02_19_114456_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('s_nmae');
+            $table->integer('s_ref');
+            $table->string('e_mail');
+            $table->integer('workshop_id')->unsigned();
+            //$table->foreign('workhop_id')->refences('id')->on('workshops');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
